e2c89eaae233a61ab277705875d930c736cb1e15
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-02-13 13:50:30 +0600

    Create pageView

---
+++ Page View/pageView.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/page1.dart';
+import 'package:test_app/page2.dart';
+import 'package:test_app/page3.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: PageView(
+          scrollDirection: Axis.vertical,
+          controller: _controller,
+          children: <Widget>[
+            page1(),
+            page2(),
+            page3(),
+          ],
+        ),
+      ),
+    );
+  }
+}
