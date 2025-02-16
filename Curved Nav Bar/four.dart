c735fd44037961e5e671bf6c2e37267e69c00989
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-02-16 18:02:26 +0600

    Create four.dart

---
+++ Curved Nav Bar/four.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class four extends StatelessWidget {
+  const four({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text('four', style: TextStyle(fontSize: 50),),
+      ),
+    );
+  }
+}
+
