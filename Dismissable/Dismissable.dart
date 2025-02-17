db75f9a58e03e4f813a0e847b37eb5e54e667ac7
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-02-17 14:05:04 +0600

    Create Dismissable

---
+++ Dismissable/Dismissable.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Modal button',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  String? selected = "Choose";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Dismissible(
+          key: ValueKey('ABC'),
+          secondaryBackground: Container(
+            color: Colors.blue,
+            child: Icon(Icons.archive, size: 40,),
+          ),
+          background: Container(
+            color: Colors.red,
+            child: Icon(Icons.delete, size: 40,),
+          ),
+          child: ListTile(
+            title: Text('this is list tile title'),
+            subtitle: Text('this is list tile subtitle'),
+            // trailing: Icon(Icons.ac_unit),
+            leading: Icon(Icons.verified_user),
+          ),
+        )
+      ),
+    );
+  }
+}
