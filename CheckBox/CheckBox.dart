db9d580dd3e5d3621f7c5f5618dbfc3a200c04dd
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-02-17 14:15:29 +0600

    Create CheckBox

---
+++ CheckBox/CheckBox.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Modal button',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  bool _value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_value == false? 'Unchecked': 'Checked'),
+            Checkbox(
+                value: _value,
+                onChanged: (bool? val){
+                  setState(() {
+                    _value = val ?? _value;
+                    print(_value);
+                  });
+                })
+          ],
+        ),
+      )
+    );
+  }
+}
