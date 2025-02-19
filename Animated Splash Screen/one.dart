3a57ba40211ea2c1bd69c22a8e047814da1f161f
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-02-19 11:18:21 +0600

    Create one

---
+++ Animated Splash Screen/one.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class one extends StatelessWidget {
+  const one({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: Center(
+        child: Text('one', style: TextStyle(fontSize: 50),),
+      ),
+    );
+  }
+}
