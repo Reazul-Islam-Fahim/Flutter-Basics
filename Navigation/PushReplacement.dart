4aeca10719fcc3484806b10a40a78948e62940af
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-02-20 18:55:44 +0600

    Create PushReplacement

---
+++ Navigation/PushReplacement.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() {
+  runApp(DevicePreview(builder: (_) => MyApp(),));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/first':(context) => FirstSreen(),
+        '/second':(context) => SecondScreen(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstSreen(),
+    );
+  }
+}
+
+class FirstSreen extends StatefulWidget {
+  const FirstSreen({super.key});
+
+  @override
+  State<FirstSreen> createState() => _FirstSreenState();
+}
+
+class _FirstSreenState extends State<FirstSreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: OutlinedButton(
+            onPressed: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SecondScreen(),),);
+            },
+            child: Text('fisrt screen')
+        ),
+      ),
+    );
+  }
+}
+
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: OutlinedButton(
+            onPressed: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>FirstSreen(),),);
+            },
+            child: Text('second screen')
+        ),
+      ),
+    );
+  }
+}
+
